7094f090e8302c9e70790dc728d9e52cf025fbc9
Minh Bao <minhbao1223@gmail.com>
2022-03-03 16:38:53 +0700

    first commit

---
+++ codeMau.cpp
@@ -0,0 +1,64 @@
+// 1 số mẫu test: => random 1 mảng 1 chiều có số lượng phần tử từ ít tới nhiều
+// 10 phần tử, 100 phần tử, 1,000, 10,000, 100,000 phần tử
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>
+#define MAXN 500000
+using namespace std;
+
+float ThuatToan1(int a[], int n) {
+    clock_t begin;
+    clock_t end;
+    begin = clock(); 
+    int maxSum = 0;
+    for(int i = 0; i < n; i++ )
+        for(int j = i; j < n; j++) {
+            int thisSum = 0;
+            for(int k = i; k <= j; k++ )
+                thisSum += a[k];
+            if (thisSum > maxSum)
+                maxSum = thisSum;
+        }
+    end = clock(); 
+    return ((float)end-begin)/CLOCKS_PER_SEC ;
+}
+
+float ThuatToan2(int a[], int n) {
+    clock_t begin;
+    clock_t end;
+    begin = clock(); 
+    int maxSum = 0, thisSum = 0;
+    for( int j = 0; j < n; j++ ) {
+        thisSum += a[j];
+        if ( thisSum > maxSum )
+            maxSum = thisSum;
+        else if ( thisSum < 0 )
+            thisSum = 0;
+    }
+    end = clock(); 
+    return ((float)end-begin)/CLOCKS_PER_SEC ;
+}
+
+void RandomArray(int a[], int n){
+    srand (time(NULL));
+    for(int i=0 ; i< n ; i++){
+         a[i]=rand()%1000-600; // Ghi chú tạo ds có số âm, số dương
+    }
+}
+int main(){
+    int a[MAXN], n, t1 ,t2;
+    int test[10]={10, 100, 1000, 10000, 100000, 200000, 300000, 400000, 500000}, ntest=5;
+    cout << "Hello" << endl ;
+    for(int i=0  ; i < ntest ; i++){
+        n=test[i];
+        RandomArray(a, n);
+        t1=ThuatToan1(a,n); 
+        // t2=ThuatToan2(a, n); 
+        cout << "Thoi gian: " << t1 << endl; ;
+    }
+    
+    return 0;
+}
+
